80a3d655cce32ce596f07b004eb86e262d7e7cd3
darklord4822 <darklord4722@gmail.com>
2018-03-03 23:49:47 +0100

    mt6737-common: patches: introduce new patches for Nougat

--- patches/install.sh
+++ patches/install.sh
@@ -2,23 +2,13 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="frameworks/av"
+dirs="frameworks/av frameworks/base frameworks/native system/core system/netd"
 
 for dir in $dirs ; do
 	cd $rootdirectory
 	cd $dir
 	echo "Applying $dir patches..."
-	git am $rootdirectory/device/cyanogen/mt6755-common/patches/$dir/*.patch
-	echo " "
-done
-
-dirs="frameworks/base"
-
-for dir in $dirs ; do
-	cd $rootdirectory
-	cd $dir
-	echo "Applying $dir patches..."
-	git am $rootdirectory/device/cyanogen/mt6755-common/patches/$dir/*.patch
+	git am $rootdirectory/device/mediatek/mt6737-common/patches/$dir/*.patch
 	echo " "
 done
 
